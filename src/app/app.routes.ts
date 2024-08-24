56da70de7db0805f45f56210225230a7785f1729
robertocajueiro <beto.cajueiro@gmail.com>
2024-08-24 19:05:48 -0300

    projeto inicial / aula24082024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DespesasComponent } from './relatorios/despesas/despesas.component';
+import { ReceitasComponent } from './relatorios/receitas/receitas.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: PageNotFoundComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'relatorio/despesa', component: DespesasComponent },
+  { path: 'relatorio/receita', component: ReceitasComponent },
+
+
+
+];
